37827a6b70a97c5d5221e6f23a96a58113a1a8be
Mohammad Iqbal <mi215704@gmail.com>
2025-08-16 15:51:49 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,23 @@
 import './global.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: "IABXXI",
   description: "Web forum diskusi hanya untuk senang-senang.",
+  metadataBase: new URL("https://iabxxi.vercel.app/"),
   openGraph: {
     title: 'IABXXI',
     description: 'Forum diskusi untuk semua orang.',
+    url: 'https://iabxxi.vercel.app/',
     images: ['/og-image.png'],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "IABXXI",
+    description: "Forum diskusi.",
+    images: ["/og-image.png"],
   },
 };
 
